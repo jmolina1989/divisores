f9ac55dc6d83a78018a69ed1326ab4faf5d1851e
Jose Molina Tejera <80555671+jmolina1989@users.noreply.github.com>
2022-02-14 00:24:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const divisor = () => {
+    let numero = document.getElementById('numero').value;
+    if (numero!="" && numero <= 100000000){
+        if (numero==0){
+            document.getElementById('divisores').innerHTML = "El 0 tiene infinitos divisores exceptuando al mismo 0";
+            document.getElementById('cantidad').innerHTML = "";
+        }else{
+            numero = parseInt(numero);
+            const divisores = [];
+            let suma = 0;
+            for (let i = 1; i <= numero; i++) {
+                if (numero%i==0){
+                    divisores.push(i)
+                    suma += 1;
+                }
+            }
+            document.getElementById('divisores').innerHTML = `D<sub>${numero}</sub> = {${divisores}}`;
+            document.getElementById('cantidad').innerHTML = `El numero ${numero} tiene ${suma} divisores`;
+        }
+    }else {
+        document.getElementById('divisores').innerHTML = "Debe escribir un numero entre 1 y 100.000.000";
+        document.getElementById('cantidad').innerHTML = "";
+    }
+}
